6dc184394444a7cc19f4f06caf296c0c730bcb19
Ranwiesiel <ronggofc@gmail.com>
2023-07-12 00:48:52 +0700

    fixed search

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var input = document.getElementById('input');
 
-document.getElementById('button').onclick = function() {
+document.getElementById('button').addEventListener("click", function() {
     open('http://google.com/search?q='+input.value, '_self');
-};
+});
 
-document.getElementById('input').addEventListener('keypress', function(event) {\
-    if (event.key === 'Enter') {
+document.getElementById('input').addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
         event.preventDefault();
         document.getElementById('button').click();
     }
